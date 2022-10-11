19502553e3659adffe93f2faaad8bf3872ccdd4b
Sara Brumfield <sarabrumfield@gmail.com>
2022-10-11 10:25:47 -0400

    funding for MAP for laura

--- 4_map_fund_level.R
+++ 4_map_fund_level.R
@@ -1,7 +1,9 @@
+.libPaths("C:/Users/sara.brumfield2/OneDrive - City Of Baltimore/Documents/r_library")
 library(stringr)
 library(stringi)
 library(tidyr)
 library(openxlsx)
+
 devtools::load_all("G:/Analyst Folders/Sara Brumfield/bbmR")
 
 df <- readxl::read_excel("G:/Analyst Folders/Sara Brumfield/exp_planning_year/2c_proposals_results_teams/outputs/MAP Action Detail from SharePoint.xlsx", sheet = "SharePoint") %>%
@@ -17,10 +19,9 @@ separate(col = "City Service:Title", into = c("Service Name 1", "Service Name 2"
 df2 <- readxl::read_excel("G:/Fiscal Years/Fiscal 2024/Planning Year/2. Prop/Agency Guidance/MAP Action Detail_FY23 Funding.xlsx", sheet = "Sheet1") %>%
   mutate(`Action Plan Item Number` = as.character(`Action Plan Item Number`))
 
-df3 <- left_join(df2, df %>% select(`Service IDs`, `Service Name`, `City Agency`, `Action`), by = "Action")
+df3 <- left_join(df2, df %>% select(`Service IDs`, `Service Name`, `City Agency`, `Action`, `ActionOwnerLookupId`), by = "Action")
 
-
-
+write.xlsx(df3, file = "G:/Fiscal Years/Fiscal 2024/Planning Year/2. Prop/Agency Guidance/MAP Action Detail with Services_FY23 Funding.xlsx")
 
 
 #fuck this shit======
@@ -48,31 +49,31 @@ df3 <- left_join(df2, df %>% select(`Service IDs`, `Service Name`, `City Agency`
 
 
 
-df2 <- df %>% 
-  unite("Service IDs", `Service 1`:`Service 7`, sep = ",", remove = TRUE, na.rm = TRUE) %>%
-  separate(col = "Service IDs", into = c("Service ID 1", "Service ID 2", "Service ID 3", "Service ID 4", sep = ", ", remove = FALSE)) %>%
-  mutate( `Service Name` = trimws(gsub('[0-9]+,', '', `Service Name`), which = "left"),
-         `City Service:Agency` = trimws(gsub('[0-9]+,', '', `City Service:Agency`), which = "left")) %>%
-  separate(col = "Service Name", into = c("Service Name 1", "Service Name 2", "Service Name 3",
-                                          "Service Name 4"), sep = ", ", remove = TRUE)
-
-df3 <- df2 %>% group_by(`PillarText`, `Goal`, `Action Plan Item Number`, `Action`, `Status`,
-                        `Service ID 1`, `Service Name 1`, `Service ID 2`, `Service Name 2`,
-                        `Service ID 3`, `Service Name 3`, `Service ID 4`, `Service Name 4`) %>%
-  summarise(`FY23 Needed` = sum(`FY23_Funding_Needed`, na.rm = TRUE),
-            `FY23 Funded` = sum(`FY23_Funding_Identified`, na.rm = TRUE)) %>%
-  mutate(`Fully Funded` = case_when(`FY23 Needed` <= `FY23 Funded` ~ "Yes",
-                                    TRUE ~ "No"))
-df4 <- df2 %>% group_by(`PillarText`, `Goal`, `Action Plan Item Number`, `Action`) %>%
-  summarise(`FY23 Needed` = sum(`FY23_Funding_Needed`, na.rm = TRUE),
-            `FY23 Funded` = sum(`FY23_Funding_Identified`, na.rm = TRUE)) %>%
-  mutate(`Fully Funded` = case_when(`FY23 Needed` <= `FY23 Funded` ~ "Yes",
-                                    TRUE ~ "No"))
-
-df5 <- df3 %>% group_by(`Fully Funded`) %>%
-  summarise(Count = n()) %>%
-  mutate(`Percent` = Count / sum(Count))
-
-export_excel(df3, file_name = "G:/Fiscal Years/Fiscal 2024/Planning Year/2. Prop/Agency Guidance/Funding by MAP.xlsx", tab_name = "FY23 Funding by Goal and Action")
-export_excel(df4, file_name = "G:/Fiscal Years/Fiscal 2024/Planning Year/2. Prop/Agency Guidance/Funding by MAP.xlsx", tab_name = "Funding without Service IDs", type = "existing")
-export_excel(df5, file_name = "G:/Fiscal Years/Fiscal 2024/Planning Year/2. Prop/Agency Guidance/Funding by MAP.xlsx", tab_name = "Percent Funded Crosstab", type = "existing")
+# df2 <- df %>% 
+#   unite("Service IDs", `Service 1`:`Service 7`, sep = ",", remove = TRUE, na.rm = TRUE) %>%
+#   separate(col = "Service IDs", into = c("Service ID 1", "Service ID 2", "Service ID 3", "Service ID 4", sep = ", ", remove = FALSE)) %>%
+#   mutate( `Service Name` = trimws(gsub('[0-9]+,', '', `Service Name`), which = "left"),
+#          `City Service:Agency` = trimws(gsub('[0-9]+,', '', `City Service:Agency`), which = "left")) %>%
+#   separate(col = "Service Name", into = c("Service Name 1", "Service Name 2", "Service Name 3",
+#                                           "Service Name 4"), sep = ", ", remove = TRUE)
+# 
+# df3 <- df2 %>% group_by(`PillarText`, `Goal`, `Action Plan Item Number`, `Action`, `Status`,
+#                         `Service ID 1`, `Service Name 1`, `Service ID 2`, `Service Name 2`,
+#                         `Service ID 3`, `Service Name 3`, `Service ID 4`, `Service Name 4`) %>%
+#   summarise(`FY23 Needed` = sum(`FY23_Funding_Needed`, na.rm = TRUE),
+#             `FY23 Funded` = sum(`FY23_Funding_Identified`, na.rm = TRUE)) %>%
+#   mutate(`Fully Funded` = case_when(`FY23 Needed` <= `FY23 Funded` ~ "Yes",
+#                                     TRUE ~ "No"))
+# df4 <- df2 %>% group_by(`PillarText`, `Goal`, `Action Plan Item Number`, `Action`) %>%
+#   summarise(`FY23 Needed` = sum(`FY23_Funding_Needed`, na.rm = TRUE),
+#             `FY23 Funded` = sum(`FY23_Funding_Identified`, na.rm = TRUE)) %>%
+#   mutate(`Fully Funded` = case_when(`FY23 Needed` <= `FY23 Funded` ~ "Yes",
+#                                     TRUE ~ "No"))
+# 
+# df5 <- df3 %>% group_by(`Fully Funded`) %>%
+#   summarise(Count = n()) %>%
+#   mutate(`Percent` = Count / sum(Count))
+# 
+# export_excel(df3, file_name = "G:/Fiscal Years/Fiscal 2024/Planning Year/2. Prop/Agency Guidance/Funding by MAP.xlsx", tab_name = "FY23 Funding by Goal and Action")
+# export_excel(df4, file_name = "G:/Fiscal Years/Fiscal 2024/Planning Year/2. Prop/Agency Guidance/Funding by MAP.xlsx", tab_name = "Funding without Service IDs", type = "existing")
+# export_excel(df5, file_name = "G:/Fiscal Years/Fiscal 2024/Planning Year/2. Prop/Agency Guidance/Funding by MAP.xlsx", tab_name = "Percent Funded Crosstab", type = "existing")
